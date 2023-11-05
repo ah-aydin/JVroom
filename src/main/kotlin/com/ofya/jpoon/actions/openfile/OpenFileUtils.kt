6c6216aa4d3c9627c4f904071223609eca9d208b
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2023-11-05 14:16:29 +0100

    Refactor

--- src/main/kotlin/com/ofya/jpoon/actions/openfile/OpenFileUtils.kt
+++ src/main/kotlin/com/ofya/jpoon/actions/openfile/OpenFileUtils.kt
@@ -10,17 +10,17 @@ import com.ofya.jpoon.settings.SettingsState
 fun openFile(event: AnActionEvent, index: Int) {
     val project = event.project ?: return
     val settingsState = SettingsState.getInstance()
+    val fileEditorManager = FileEditorManager.getInstance(project)
 
     val fileToOpenPath = project.service<GlobalStateService>().getFilePathAt(index) ?: return
     val fileToOpen = VirtualFileManager.getInstance().findFileByUrl("file://$fileToOpenPath")
 
+
     if (fileToOpen != null) {
-        val fileEditorManager = FileEditorManager.getInstance(project)
         fileEditorManager.openFile(fileToOpen, true)
     }
 
     if (settingsState.closeFilesAfterOpenFile) {
-        val fileEditorManager = FileEditorManager.getInstance(project)
         fileEditorManager.openFiles.forEach { openFile ->
             run {
                 if (openFile == fileToOpen) {

40f1a7b0491b14bccdf5a4b9b95dba12531c4b24
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2024-03-23 10:15:20 +0100

    Increase compatiblity version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.ofya"
-version = "2024-02-10"
+version = "2024-03-23"
 
 repositories {
   mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("222")
-    untilBuild.set("233.*")
+    untilBuild.set("241.*")
   }
 
   signPlugin {
